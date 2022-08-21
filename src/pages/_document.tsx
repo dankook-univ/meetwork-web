a047ef2c97effe0966acb901c021bf0d4df0a707
himitery <himit0131@gmail.com>
2022-08-21 18:12:35 +0900

    feat: 로고 및 favicon 적용

---
+++ src/pages/_document.tsx
@@ -0,0 +1,37 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+const Document = () => {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { GetServerSideProps, NextPage } from 'next';
+import Image from 'next/image';
 
 import { withoutAuthSSR } from '@/utils/session/withoutAuth';
 
@@ -12,13 +13,19 @@ interface IndexProps {
 const Index: NextPage<IndexProps> = ({ kakaoClientId }) => {
   return (
     <div className="flex flex-col min-w-screen min-h-screen p-[26px] items-center justify-center bg-gradient-to-b from-mint to-primary">
-      <section className="flex flex-1 items-center justify-center">
-        <span>로고</span>
-      </section>
+      <div className="flex flex-col items-center">
+        <Image
+          className="rounded-[20px]"
+          src="/icons/logo.svg"
+          width={130}
+          height={130}
+          alt=""
+        />
 
-      <section className="flex flex-1 items-center justify-center">
-        <KakaoLoginButton kakaoClientId={kakaoClientId} />
-      </section>
+        <div className="mt-[160px]">
+          <KakaoLoginButton kakaoClientId={kakaoClientId} />
+        </div>
+      </div>
     </div>
   );
 };
